eb8fff8ad7b58e2c67f33c498d7ba1448e59e6ee
Greg15153 <4452658+Greg15153@users.noreply.github.com>
2020-02-21 01:51:38 +0000

    Switched to NestJS with SSR

--- src/app/server/users/errors.ts
+++
@@ -1,8 +0,0 @@
-import { RequiredStringError } from '../util/errors'
-
-export type FirstNameRequiredError = RequiredStringError
-export type LastNameRequiredError = RequiredStringError
-
-export type ValidationError = FirstNameRequiredError | LastNameRequiredError
-
-export type NewUserError = ValidationError[]
