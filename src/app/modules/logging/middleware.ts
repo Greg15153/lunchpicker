2856a23dbf2fcffa1542f3919c2938467ac1ebfe
Greg15153 <4452658+Greg15153@users.noreply.github.com>
2020-02-29 02:25:08 +0000

    Utilize Fastify instead of Express

--- src/app/modules/logging/middleware.ts
+++ src/app/modules/logging/middleware.ts
@@ -1,20 +1,38 @@
+import { IncomingMessage, ServerResponse } from 'http'
+
 import { Injectable, NestMiddleware, Scope } from '@nestjs/common'
+import { v4 as uuid } from 'uuid'
 
 import LoggerService from './logger-service'
+
+interface FastifyRequest extends IncomingMessage {
+    id: number
+    ip: string
+}
 
 @Injectable({ scope: Scope.REQUEST })
 class LoggerMiddleware<TRequest, TResponse> implements NestMiddleware {
     public constructor(private readonly logger: LoggerService) {}
 
-    use(req: Request, res: Response, next: Function): void {
+    async use(req: FastifyRequest, res: ServerResponse, next: Function): Promise<void> {
+        const start = new Date().getTime()
         const msg = {
             url: req.url,
-            headers: req.headers,
-            method: req.method
+            method: req.method,
+            id: req.id,
+            ip: req.ip,
+            traceId: req.headers['x-request-id'] ?? uuid()
         }
         this.logger.log(JSON.stringify(msg))
-        next()
-        this.logger.log(JSON.stringify({ ...msg, status: res.status }))
+
+        await next()
+        this.logger.log(
+            JSON.stringify({
+                ...msg,
+                status: res.statusCode || req.statusCode,
+                elapsed: new Date().getTime() - start
+            })
+        )
     }
 }
 
--- src/app/server/main.ts
+++ src/app/server/main.ts
@@ -1,25 +1,25 @@
 import { NestFactory } from '@nestjs/core'
-import { NestExpressApplication } from '@nestjs/platform-express'
-import register from '@react-ssr/nestjs-express/register'
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
+import fastify from 'fastify'
 import LoggerService from 'modules/logging/logger-service'
 
 import { AppModule } from './app-module'
 
+const logger = new LoggerService()
+logger.setContext('Main')
 ;(async (): Promise<void> => {
-    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    const serverOptions: fastify.ServerOptionsAsHttp = {
+        ignoreTrailingSlash: true,
+        requestIdHeader: 'X-Request-Id'
+    }
+    const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(serverOptions), {
         logger: process.env.NODE_ENV?.toLowerCase() === 'development'
     })
-
-    const logger = await app.resolve(LoggerService)
-    logger.setContext('Main')
     app.useLogger(logger)
 
-    // register `.tsx` as a view template engine
-    await register(app)
-
-    app.listen(3000, async () => {
+    app.listen(3000, '0.0.0.0', async () => {
         logger.log('Ready on http://localhost:3000')
     })
 })().catch(ex => {
-    console.log(ex)
+    logger.error(ex)
 })
